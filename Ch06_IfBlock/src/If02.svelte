f7e3392a705bdff119578d5b94db30dcdfde6eba
유재혁 <yjk7454@naver.com>
2026-02-18 23:00:15 +0900

    feat: `Ch06 IfBlock` - 마크업 조건문 IfBlock {#if}실습 예제

---
+++ Ch06_IfBlock/src/If02.svelte
@@ -0,0 +1,14 @@
+<script>
+  let user = { loggedState: false }
+  const onToggle = () => user.loggedState = !user.loggedState
+</script>
+<div>
+  <h1>01) If Block 버튼전환 예제</h1>
+  {#if user.loggedState}
+    <button on:click={onToggle}>로그아웃</button>
+    <p>현재 로그인 상태입니다.</p>
+  {:else}
+    <button on:click={onToggle}>로그인</button>
+    <p>현재 로그아웃 상태입니다.</p>
+  {/if}
+</div>
---
+++ Ch06_IfBlock/src/If03.svelte
@@ -0,0 +1,25 @@
+<script>
+  let x = null
+  const numChange = (event) => {
+    x = event.target.value
+  }
+</script>
+<div>
+  <h1>03) If~Else IF~Else Block 양수/음수 예제</h1>
+  <label for="testBox">양수/음수 테스트</label>
+  <input
+    type="text" 
+    id="textBox" 
+    placeholder="정수를 입력하세요."
+    on:keyup={numChange} 
+  >
+  {#if x > 0}
+    <p>작성한 숫자는 양수입니다.</p>
+  {:else if x < 0}
+    <p>작성한 숫자는 음수입니다.</p>
+  {:else if x == 0}
+    <p>작성한 숫자는 0입니다.</p>
+  {:else}
+    <p>정수로 다시 입력하세요.</p>
+  {/if}
+</div>
