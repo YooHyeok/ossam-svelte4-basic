fc1e00ebfb3052be1ab45331f73c97cbe18b586d
유재혁 <yjk7454@naver.com>
2026-02-26 00:27:32 +0900

    feat: `Ch09 Bind02` - Media bind 실습 예제

--- Ch09_Bind02/src/App.svelte
+++ Ch09_Bind02/src/App.svelte
@@ -2,7 +2,9 @@
 	import Select01 from "./Select01.svelte";
 	import Select02 from "./Select02.svelte";
 	import TextArea from "./TextArea.svelte";
+	import Media from "./Media.svelte";
 </script>
 <Select01/>
 <Select02/>
 <TextArea/>
+<Media/>
