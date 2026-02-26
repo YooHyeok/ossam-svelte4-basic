5cf91a403f3515c80aab3982c25427e3dff490ca
유재혁 <yjk7454@naver.com>
2026-02-27 01:50:05 +0900

    feat: `Ch08 Bind01` - each 블록 bind 실습 예제 (버킷리스트)

--- Ch08_Bind01/src/App.svelte
+++ Ch08_Bind01/src/App.svelte
@@ -4,10 +4,11 @@
 	import Input03 from "./Input03.svelte";
 	import Input04 from "./Input04.svelte";
 	import Input05 from "./Input05.svelte";
-
+	import BucketList from "../BucketList.svelte";
 </script>
 <Input01/>
 <Input02/>
 <Input03/>
 <Input04/>
 <Input05/>
+<BucketList/>
---
+++ Ch08_Bind01/src/BucketList.svelte
@@ -0,0 +1,30 @@
+<script>
+  let buckets = [
+    { chk: false, text: '웹 풀스택 개발자 되기' },
+    { chk: false, text: '유럽여행가기' },
+    { chk: false, text: '영국가서 손흥민 축구보기' }
+  ];
+  $: remaining = buckets.filter(bucket => !bucket.chk).length
+  const onAdd = () => {
+    buckets = [...buckets, { chk: false, text: '' }] // 가장 성능이 좋음
+    // buckets = buckets.concat({ chk: false, text: '' })
+    // buckets.push({ chk: false, text: '' }) // push는 성능에 좋지않음
+    // buckets = buckets
+  }
+  const onRemove = () => {
+    buckets = buckets.filter(bucket => !bucket.chk)
+  }
+</script>
+<div>
+  <h1>04) each블록 바인드 예제 - 버킷리스트</h1>
+  {#each buckets as bucket}
+  <div>
+    <input type="checkbox" bind:checked={bucket.chk}/>
+    <input type="text" placeholder="당신의 버킷리스트는 뭔가요?" bind:value={bucket.text} style="width: 250px">
+  </div>
+  {/each}
+
+  <p>남은 버킷리스트 : {remaining}</p>
+  <button on:click={onAdd}>새로운 버킷 추가</button>
+  <button on:click={onRemove}>성공한 버킷 제거</button>
+</div>
