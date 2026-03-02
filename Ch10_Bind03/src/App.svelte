2ebd803ff2eb5eefba6328fab8b45708715a4057
유재혁 <yjk7454@naver.com>
2026-03-02 21:32:29 +0900

    feat: `Ch10 Bind03` - Dimension(공간) 읽기전용 bind 실습 예제

--- Ch10_Bind03/src/App.svelte
+++ Ch10_Bind03/src/App.svelte
@@ -1,6 +1,8 @@
 <script>
 	import This from "./bind/this/This.svelte";
 	import Parent from "./bind/component/Parent.svelte";
+	import Dimension from "./bind/dimension/Dimension.svelte";
 </script>
 <This/>
 <Parent/>
+<Dimension/>
---
+++ Ch10_Bind03/src/bind/dimension/Dimension.svelte
@@ -0,0 +1,20 @@
+<script>
+  let w; // div 태그의 폭
+  let h; // div 태그의 높이
+  let size = 20; // 글자 크기
+</script>
+
+<p>슬라이드바로 글자 크기를 변경해보세요.</p>
+<input type="range" min="10" max="100" bind:value={size} />
+<p>글자 크기 : {size}px</p>
+<div bind:clientWidth={w} bind:clientHeight={h}>
+    <span style="font-size: {size}px;">글자</span>
+</div>
+<ul>
+    <li>가로 폭 : {w}</li>
+    <li>세로 높이 : {h}</li>
+</ul>
+
+<style>
+    div{ display: inline-block; border: 3px solid black; }
+</style>
