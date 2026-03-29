d26699c87ac2603a786231a6a1383a0d3421b11d
유재혁 <yjk7454@naver.com>
2026-03-29 17:48:56 +0900

    docs: `README.md` - Ch19_Transition Scale & Fly 개념 및 예제

--- Ch19_Transition/src/App.svelte
+++ Ch19_Transition/src/App.svelte
@@ -1,9 +1,9 @@
 <script>
 	import Fade from "./01)fade/Fade.svelte";
-	import Blur from "./02_01)blur/Blur.svelte";
-	import Slide from "./02_02)slide/Slide.svelte";
+	import Blur from "./02)blur&slide/01)blur/Blur.svelte";
+	import Slide from "./02)blur&slide/02)slide/Slide.svelte";
 	import Scale from "./03)scale&fly/01)scale/Scale.svelte";
-		import Fly from "./03)scale&fly/02)fly/Fly.svelte";
+	import Fly from "./03)scale&fly/02)fly/Fly.svelte";
 </script>
 <Fade/>
 <Blur/>
