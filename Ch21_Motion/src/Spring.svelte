0d00aceaf52657c20d06a28a7bcbae8f3fb9b83c
유재혁 <yjk7454@naver.com>
2026-04-03 08:41:50 +0900

    feat: `Ch21_Motion - tweened, spring 효과 예제

---
+++ Ch21_Motion/src/Spring.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { spring } from 'svelte/motion'
+    const coords = spring(
+      { x: 50, y: 50 },
+      {
+        stiffness: 0.1,
+        damping: 0.25,
+      }
+    )
+    // let x, y;
+</script>
+
+<!-- <svg on:mousemove={(e) => { x = e.clientX; y = e.clientY; }}>
+    <circle cx={x} cy={y} r="10" />
+</svg> -->
+<svg on:mousemove={(e) => { coords.set({ x: e.clientX, y: e.clientY })}}>
+    <circle cx={$coords.x} cy={$coords.y} r="10" />
+</svg>
+
+<style>
+    svg{ width: 100%; height: 100%; margin: -8px; }
+    circle{ fill: #ff3e00; }
+</style>
