0a3877232f9796cdc31aba7188507d708a2fb3c0
유재혁 <yjk7454@naver.com>
2026-04-18 22:47:14 +0900

    feat: `Ch27_HttpAwaitBlock - Axios와 each~else 블록 예제

---
+++ Ch27_HttpAwaitBlock/src/AxiosAwaitBlock.svelte
@@ -0,0 +1,31 @@
+<script>
+  import axios from 'axios'
+
+  const callAxios = async () => {
+  try {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/comments?_limit=21`);
+    return res.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+let promise = callAxios();
+  
+</script>
+
+<div>
+  <h2>Axios Await Block</h2>
+  {#await promise}
+  <p>loading...</p>
+  {:then value}
+  {#each value as comment}
+  <article>
+    <h4>이름 : {comment.name}</h4>
+    <h4>이메일 주소 : {comment.email}</h4>
+  </article>
+  {/each}
+  {:catch error}
+  <p>{error.message} : 에러가 발생되었습니다.</p>
+  {/await}
+</div>
---
+++ Ch27_HttpAwaitBlock/src/FetchEachElse.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let comments = [];
+
+  const callFetch = async () => {
+    try {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/comments?_limit=21`);
+      comments = await res.json();
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+  onMount(async () => {
+    await callFetch();
+  });
+  
+</script>
+<div>
+  <h2>Fetch Each Else Block</h2>
+  {#each comments as comment}
+      <article>
+          <h4>이름 : {comment.name}</h4>
+          <h4>이메일 주소 : {comment.email}</h4>
+      </article>
+  {:else}
+      <p>loading...</p>
+  {/each}
+</div>
