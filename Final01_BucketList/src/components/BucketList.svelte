2aca08a3aadd7071643a5bade15f43e2a282b104
유재혁 <yjk7454@naver.com>
2026-04-23 08:37:26 +0900

    feat: `Final01_BucketList - 버킷리스트 화면전환 효과 적용

--- Final01_BucketList/src/components/BucketList.svelte
+++ Final01_BucketList/src/components/BucketList.svelte
@@ -1,5 +1,9 @@
 <script>
+  import { fade, slide } from "svelte/transition"
+  import { flip } from "svelte/animate";
+
   import BucketItem from "./BucketItem.svelte";
+
   export let buckets;
   export let onToggle;
   export let onRemove;
@@ -7,12 +11,32 @@
   export let editMode;
   export let onEditMode;
   export let onEditKeyup;
+
+  const blink = (node, value) => {
+    let prev = value;
+    return {
+      update: (newValue) => {
+        if (prev !== newValue) {
+          node.animate([
+            { opacity: 1 },
+            { opacity: 0.3 },
+            { opacity: 1 }
+          ], { duration: 300 });
+        }
+        prev = newValue;
+      }
+    }
+  }
 </script>
 <div class="bucketlist">
   {#each buckets as bucket (bucket.id)}
-    <BucketItem 
-      {bucket} {onToggle} {onRemove} 
-      {editMode} {onEditMode} {onEditKeyup}
-    />
-  {/each}
+  <!-- <div in:fade out:slide animate:flip> --> <!-- key가 bucket이고 체크 토글시 map으로 재할당할 경우 -->
+  <div in:fade out:slide use:blink={bucket.chk}> <!-- key가 bucket.id인 경우 flip 작동 안함 -->
+      <BucketItem 
+        
+        {bucket} {onToggle} {onRemove}
+        {editMode} {onEditMode} {onEditKeyup}
+      />
+  </div>
+{/each}
 </div>
