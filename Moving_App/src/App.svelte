d910ac2af8131da36e12d22bcca07c648af2df0e
유재혁 <yjk7454@naver.com>
2026-05-02 20:33:23 +0900

    feat: `Moving_App`  - TMDB 영화 지금 재생 중 목록 구현

--- Moving_App/src/App.svelte
+++ Moving_App/src/App.svelte
@@ -1,30 +1,49 @@
 <script>
-	export let name;
-</script>
+	import axios from 'axios';
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	const options = {
+		method: 'GET',
+		url: 'https://api.themoviedb.org/3/movie/now_playing',
+		params: {language: 'ko', page: '1'},
+		headers: {
+			accept: 'application/json',
+			Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDdjYzYzMzBhZGRlNDk4OWEzMTRjYTk4NjZjNWM0YSIsIm5iZiI6MTcwOTA0ODU3OC4xMTM5OTk4LCJzdWIiOiI2NWRlMDMwMmE4OTRkNjAxODcwZDgzYjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Uq_kyfrmNB3wyRJIQKmw7L9MI0E4sdx3al8c_N1t8QU'
 		}
+	};
+
+	let movies = [];
+
+	const getMovies = async () => {
+		try {
+			const res = await axios.request(options)
+			movies = await res.data.results
+			console.log(movies)
+			return res
+		} catch (error) {
+			throw new Error(error)
+		}
+	}
+
+	let promise = getMovies();
+
+</script>
+{#await promise}
+	 <p>loading...</p>
+{:then value}
+	{#each value.data.results as movie}
+  <article>
+    <h3>영화명 : {movie.title}</h3>
+		<p>설명: {movie.overview}</p>
+		<img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}/>
+		<br/>
+  </article>
+  {/each}
+{:catch error}
+	<p>{error.message} : 에러가 발생되었습니다.</p>
+{/await}
+<style>
+	article {
+		width: 500px; border: 3px solid black;
+		padding: 20px; margin-bottom: 20px;
 	}
 </style>
