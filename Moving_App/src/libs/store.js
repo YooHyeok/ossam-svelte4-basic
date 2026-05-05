e45cd69e90a64312031670fc6da5a88917911e72
유재혁 <yjk7454@naver.com>
2026-05-06 08:59:05 +0900

    feat: `Moving_App`  - 실전 프로젝트 store 기능(tmdb 조회) 구현

---
+++ Moving_App/src/libs/store.js
@@ -0,0 +1,94 @@
+import axios from "axios"
+import { writable } from "svelte/store"
+
+const setPromise = (url) => {
+  const options = {
+    method: 'GET',
+    url,
+    params: {
+      language: 'ko',
+      page: '1'
+    },
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDdjYzYzMzBhZGRlNDk4OWEzMTRjYTk4NjZjNWM0YSIsIm5iZiI6MTcwOTA0ODU3OC4xMTM5OTk4LCJzdWIiOiI2NWRlMDMwMmE4OTRkNjAxODcwZDgzYjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Uq_kyfrmNB3wyRJIQKmw7L9MI0E4sdx3al8c_N1t8QU'
+    }
+  };
+  const getPromise = async () => {
+    try {
+      const res = await axios.request(options)
+      return res;
+    } catch (error) {
+      throw new Error(errror)
+    }
+  }
+  const { subscribe } = writable(getPromise())
+  return {
+    subscribe,
+  }
+}
+
+const setDatas = (url) => {
+  const options = {
+    method: 'GET',
+    url,
+    params: {
+      language: 'ko',
+      page: '1'
+    },
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDdjYzYzMzBhZGRlNDk4OWEzMTRjYTk4NjZjNWM0YSIsIm5iZiI6MTcwOTA0ODU3OC4xMTM5OTk4LCJzdWIiOiI2NWRlMDMwMmE4OTRkNjAxODcwZDgzYjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Uq_kyfrmNB3wyRJIQKmw7L9MI0E4sdx3al8c_N1t8QU'
+    }
+  };
+  const getDatas = async () => {
+    try {
+      const res = await axios.request(options)
+      return res;
+    } catch (error) {
+      throw new Error(errror)
+    }
+  }
+  const { subscribe } = writable(getDatas())
+  return {
+    subscribe,
+  }
+}
+
+const setGenres = (url) => {
+  const options = {
+    method: 'GET',
+    url,
+    params: {
+      language: 'ko',
+    },
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDdjYzYzMzBhZGRlNDk4OWEzMTRjYTk4NjZjNWM0YSIsIm5iZiI6MTcwOTA0ODU3OC4xMTM5OTk4LCJzdWIiOiI2NWRlMDMwMmE4OTRkNjAxODcwZDgzYjEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Uq_kyfrmNB3wyRJIQKmw7L9MI0E4sdx3al8c_N1t8QU'
+    }
+  };
+  const getGenres = async () => {
+    try {
+      const res = await axios.request(options)
+      return res;
+    } catch (error) {
+      throw new Error(errror)
+    }
+  }
+  const { subscribe } = writable(getGenres())
+  return {
+    subscribe,
+  }
+}
+
+const BASIC_URL = "https://api.themoviedb.org/3/movie/"
+
+export const nowPromise = setPromise(`${BASIC_URL}now_playing`);
+export const nows = setDatas(`${BASIC_URL}now_playing`);
+export const popularPromise = setPromise(`${BASIC_URL}popular`);
+export const populars = setDatas(`${BASIC_URL}popular`);
+export const topPromise = setPromise(`${BASIC_URL}top_rated`);
+export const tops = setDatas(`${BASIC_URL}top_rated`);
+export const upcomingPromise = setPromise(`${BASIC_URL}upcoming`);
+export const upcomings = setDatas(`${BASIC_URL}upcoming`);
+export const genres = setGenres('https://api.themoviedb.org/3/genre/movie/list');
